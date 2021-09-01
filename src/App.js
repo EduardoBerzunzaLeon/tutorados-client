cb55de97fc1ce841a08891bdfe9d09089171c9c1
unknown <eduardoberzunzal@gmail.com>
2021-09-01 15:10:22 -0500

    Styles login Screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { Toast } from 'primereact/toast';
 import PrimeReact from 'primereact/api';
 
 import 'primereact/resources/themes/bootstrap4-light-purple/theme.css';
@@ -14,38 +11,12 @@ import MainMenu from './components/mainMenu/MainMenu';
 import { Routes } from './routes/Routes';
 
 function App() {
-  const [text, setText] = useState('');
-  const toastRef = useRef();
-
   // active ripple effect
   PrimeReact.ripple = true;
 
-  const onFormSubmit = (e) => {
-    if (text) {
-      toastRef.current.show({ severity: 'info', summary: text, life: 3000 });
-    }
-
-    // clear
-    setText('');
-
-    e.preventDefault();
-  };
-
   return (
     <div className="App">
-      <MainMenu />
-      <Toast ref={toastRef} />
-
-      <form className="p-d-flex p-jc-center p-mt-6" onSubmit={onFormSubmit}>
-        <InputText value={text} onChange={(e) => setText(e.target.value)} />
-        <Button
-          type="submit"
-          label="Submit"
-          icon="pi pi-check"
-          className="p-ml-2"
-        />
-      </form>
-
+      {/* <MainMenu /> */}
       <Routes />
     </div>
   );
